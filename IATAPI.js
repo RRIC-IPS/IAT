676b9fcf79d949cba79308a1fa90b021bc9454ef
RRIC-IPS <118233825+RRICI-IPS@users.noreply.github.com>
2022-11-15 13:36:23 +0800

    Update IATAPI.js

--- IATAPI.js
+++ IATAPI.js
@@ -165,8 +165,8 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			
 			instWidth : 99, //The width of the instructions stimulus
             
-			finalText : 'Press space to continue to the next task', 
-			finalTouchText : 'Touch the bottom green area to continue to the next task',
+			finalText : 'Press space to continue to complete this assessment', 
+			finalTouchText : 'Touch the bottom green area to complete this assessment',
 
 			touchMaxStimulusWidth : '50%', 
 			touchMaxStimulusHeight : '50%', 
